508e3aa5bf91dcb0bb480d8164208d596e10c9b1
Frederick <frederick.wennborg@gmail.com>
2023-06-08 16:11:38 +0200

    sddsa

--- DataLogicLibrary/Services/MessageService.cs
+++ DataLogicLibrary/Services/MessageService.cs
@@ -44,7 +44,7 @@ namespace DataLogicLibrary.Services
                     _colorService.ConsoleWriteLineWhite($"{Environment.NewLine}Aborted simulation{Environment.NewLine}");
                     break;
                 default:
-                    _colorService.ConsoleWriteLineWhite($"{Environment.NewLine}Car is stopped.{Environment.NewLine}");
+                    _colorService.ConsoleWriteLineWhite($"{Environment.NewLine}Car is stationary.{Environment.NewLine}");
                     break;
             }
 
