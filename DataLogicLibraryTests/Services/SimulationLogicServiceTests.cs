1083d18fd6ff970954bf3ae830a16890b5771fb9
Frederick <frederick.wennborg@gmail.com>
2023-06-09 12:44:01 +0200

    added Gas_Value_Does_Not_Go_Below_Zero_After_Decrease_Status_Values() test

--- DataLogicLibraryTests/Services/SimulationLogicServiceTests.cs
+++ DataLogicLibraryTests/Services/SimulationLogicServiceTests.cs
@@ -53,10 +53,10 @@ namespace DataLogicLibraryTests.Services
                 GasValue = 20,
                 EnergyValue = 20
             };
-            var userInput = 1;
+            var userInputTurnLeft = 1;
 
             // Act
-            var result = sut.DecreaseStatusValues(userInput, status);
+            var result = sut.DecreaseStatusValues(userInputTurnLeft, status);
 
             // Assert
             Assert.IsTrue(result.EnergyValue < 20);
@@ -71,12 +71,12 @@ namespace DataLogicLibraryTests.Services
                 GasValue = 20,
                 EnergyValue = 0
             };
-            var userInput = 1;
+            var userInputTurnLeft = 1;
             var expectedEnergyValue = 0;
 
 
             // Act
-            var result = sut.DecreaseStatusValues(userInput, status);
+            var result = sut.DecreaseStatusValues(userInputTurnLeft, status);
 
             // Assert
             Assert.AreEqual(expectedEnergyValue, result.EnergyValue);
@@ -84,7 +84,7 @@ namespace DataLogicLibraryTests.Services
 
 
         [TestMethod]
-        public void Gas_Gets_Consumed_After_Update_Status_Values_And_User_Is_Not_Refueling()
+        public void Gas_Gets_Consumed_After_Decrease_Status_Values_And_Driver_Is_Not_Resting()
         {
             // Arrange
             var status = new StatusDTO()
@@ -92,17 +92,17 @@ namespace DataLogicLibraryTests.Services
                 GasValue = 20,
                 EnergyValue = 20
             };
-            var userInput = 4;
+            var userInputReverse = 4;
 
             // Act
-            var result = sut.DecreaseStatusValues(userInput, status);
+            var result = sut.DecreaseStatusValues(userInputReverse, status);
 
             // Assert
             Assert.IsTrue(result.GasValue < 20);
         }
 
         [TestMethod]
-        public void Gas_Does_Not_Get_Consumed_After_Update_Status_Values_And_User_is_Refueling()
+        public void Gas_Does_Not_Get_Consumed_After_Decrease_Status_Values_And_Driver_Is_Resting()
         {
             // Arrange
             var status = new StatusDTO()
@@ -110,14 +110,39 @@ namespace DataLogicLibraryTests.Services
                 GasValue = 20,
                 EnergyValue = 20
             };
-            var userInput = 5;
+            var userInputRefuel = 5;
+            var expectedGasValue = 20;
 
             // Act
-            var result = sut.DecreaseStatusValues(userInput, status);
+            var result = sut.DecreaseStatusValues(userInputRefuel, status);
 
             // Assert
-            Assert.IsTrue(result.GasValue == 20);
+            Assert.AreEqual(expectedGasValue, result.GasValue);
+
         }
+
+        [TestMethod]
+        public void Gas_Value_Does_Not_Go_Below_Zero_After_Decrease_Status_Values()
+        {
+            // Arrange
+            var status = new StatusDTO()
+            {
+                GasValue = 0,
+                EnergyValue = 20
+            };
+            var userInputDriveForward = 3;
+            var expectedGasValue = 0;
+
+
+            // Act
+            var result = sut.DecreaseStatusValues(userInputDriveForward, status);
+
+            // Assert
+            Assert.AreEqual(expectedGasValue, result.GasValue);
+        }
+
+
+
 
         [TestMethod]
         public void CardinalDirection_Remains_Same_When_Driving_Forward()
