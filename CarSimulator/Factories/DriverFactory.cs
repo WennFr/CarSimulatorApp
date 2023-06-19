41107e222e88437b59015eeedb73a64bd34a8cf5
Frederick <frederick.wennborg@gmail.com>
2023-06-19 09:47:30 +0200

    added factory

---
+++ CarSimulator/Factories/DriverFactory.cs
@@ -0,0 +1,26 @@
+﻿using APIServiceLibrary.DTO;
+using CarSimulator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSimulator.Factories
+{
+    public class DriverFactory
+    {
+        public Driver CreateCar(ResultDTO resultDTO)
+        {
+            return new Driver
+            {
+                Title = resultDTO.Results[0].Name.Title,
+                First = resultDTO.Results[0].Name.First,
+                Last = resultDTO.Results[0].Name.Last,
+                City = resultDTO.Results[0].Location.City,
+                Country = resultDTO.Results[0].Location.Country,
+            };
+        }
+
+    }
+}
