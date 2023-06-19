bfe620c68885ec5ed3e404a52e6fe0e662977bee
Frederick <frederick.wennborg@gmail.com>
2023-06-19 10:02:30 +0200

    addd factory interfaces

---
+++ CarSimulator/Factories/IDriverFactory.cs
@@ -0,0 +1,15 @@
+﻿using APIServiceLibrary.DTO;
+using CarSimulator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSimulator.Factories
+{
+    public interface IDriverFactory
+    {
+        Driver CreateCar(ResultsDTO resultDTO);
+    }
+}
